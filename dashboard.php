519bc93c8be10dec5f95740c05c32b9a26dd9668
lopezsam871-art <lopezsam871@gmail.com>
2026-04-17 13:36:50 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,18 @@
-<?php include 'config.php'; 
+<?php include 'connect.php'; 
 
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
 }
 ?>
+<div class="navbar">
+    <th>Dashboard</th>
+    <a href="#">Movies</a>
+    <a href="#">Rent</a>
+    <a href="#">Buy</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+    <a href="#">Service</a>
+</div>
 
 <h2>Welcome!</h2>
 <a href="logout.php">Logout</a>
